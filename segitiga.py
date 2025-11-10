9e5488ea107f94b70ca62d61368546e2a074d253
wisjayamuhalim-blip <wisjayamuhalim@gmail.com>
2025-11-10 15:26:06 +0800

    Add files via upload

---
+++ segitiga.py
@@ -0,0 +1,9 @@
+# nama : muh.alim wisjaya
+# nim: D0425327
+#mencari luas segitiga
+
+alas = int(input("masukkan alas segitiga: "))
+tinggi = int(input("masukkan tinggi segitiga: "))
+
+luas = int((alas * tinggi) / 2)
+print('luas segitiga adalah: ' , luas)
